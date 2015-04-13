87effbf33854cd0c30487c52a93352c1aa8d2128
Mark Lawlor <mwlawlor@gmail.com>
2015-04-13 11:05:01 +1000

    Update 2015-04-13T01:04:58.554Z

--- assets/main.js
+++ assets/main.js
@@ -602,3 +602,15 @@ ga('create','UA-XXXXX-X','auto');ga('send','pageview');
 
 }(jQuery));
 
+
+var fontToggle = false;
+
+function toggleFonts(){
+  if(fontToggle){
+    document.documentElement.className = 'js wf-roboto-n5-active wf-roboto-n1-active wf-robotocondensed-n7-active wf-robotocondensed-n3-active wf-active';
+  } else {
+    document.documentElement.className = 'js wf-roboto-n5-inactive wf-roboto-n1-inactive wf-robotocondensed-n7-inactive wf-robotocondensed-n3-inactive wf-inactive';
+  }
+  fontToggle = !fontToggle;
+}
+
